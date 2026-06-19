836741da49829da409d282ebda59a1fbf6e401fe
Richard Zhou <richardzhou@Richards-MacBook-Air.local>
2026-06-19 17:26:01 +1000

    code finally fucking works

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ def get_students():
     Route to fetch all students from the database
     return: Array of student objects
     """
-    return db.get_all_students()
+    return db.get_all_students(), 200
 
 
 @app.route("/students", methods=["POST"])
@@ -48,8 +48,8 @@ def update_student(student_id):
 
     # check student exists
     check = db.get_student_by_id(student_id)
-    if check: return db.update_student(student_id, sd.get("name"), sd.get("course"), sd.get("mark"))
-    return 404
+    if check is None: return {"error": "Student not found"}, 404
+    return db.update_student(student_id, sd.get("name"), sd.get("course"), sd.get("mark")), 200
 
 
 @app.route("/students/<int:student_id>", methods=["DELETE"])
@@ -60,7 +60,7 @@ def delete_student(student_id):
     """
     check = db.get_student_by_id(student_id)
     if check: return db.delete_student(student_id)
-    return 404
+    return {"error": "Student not found"}, 404
 
 
 @app.route("/stats")
@@ -68,22 +68,39 @@ def get_stats():
     """
     Route to show the stats of all student marks 
     return: An object with the stats (count, average, min, max)
+    
+
+    students = db.get_all_students()
+    marks = []
+    for student in students:
+        mark = student["mark"]
+        if mark is not None:
+            marks.append(mark)
+    count = len(marks)
+    if count == 0:
+        return jsonify({"count": 0, "average": None, "min": None, "max": None}), 200
+    else:
+        return jsonify({"count": count, "average": sum(marks)/count, "min": min(marks), "max": max(marks)}), 200
+
+    """
+    """
+    Route to show the stats of all student marks 
+    return: An object with the stats (count, average, min, max)
     """
     all_students = db.get_all_students() # either is None or not None
-    if not all_students: return 404
+    if all_students is None: return {"error": "no students exist"}, 404
 
     # check if there is a student with a mark
-    all_marks = [s["mark"] for s in students_list if s.get("mark") is not None]
+    all_marks = [s["mark"] for s in all_students if s.get("mark") is not None]
     #average = sum(all_marks)/len(all_marks)
-    if not all_marks: return 404
+    if (len(all_marks) == 0): return {"error": "no students with marks"}, 404
     stats = {
         "count": len(all_marks),
         "average": sum(all_marks) / len(all_marks),
         "min": min(all_marks),
         "max": max(all_marks),
     }
-    return jsonify(stats), 200  # replace with your implementation
-
+    return jsonify(stats), 200
 
 @app.route("/")
 def health():
